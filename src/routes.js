98b18816bd110f299da65891cec5dee6342b44ee
brenoo2018 <thaynanbreno@gmail.com>
2020-06-23 16:15:10 -0300

    criando método store no controller de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,11 @@
 // ARQUIVO ONDE FICA TODAS AS ROTAS DA APLICAÇÃO
 
 const { Router } = require('express');
-const User = require('./app/models/User');
+
+const UserController = require('./app/controllers/UserController');
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Breno',
-    email: 'breno@breno.com',
-    password_hash: '123456',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 module.exports = routes;
