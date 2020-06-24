df4ef77ed362e7c807ba2c9661be6d5fbc7b0191
brenoo2018 <thaynanbreno@gmail.com>
2020-06-24 09:52:49 -0300

    método update em UserController p/ usuário autenticado.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -26,8 +26,37 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId);
-    return res.json({ ok: true });
+    const { email, oldPassword } = req.body;
+
+    // pesquisa o usuário pela primary key
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      /**
+       * Verificando se já existe um usuário com o mesmo email
+       */
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'Email já existente.' });
+      }
+    }
+
+    /**
+     * Só altera a senha se informar a senha antiga.
+     */
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Senha incorreta.' });
+    }
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
--- src/app/middlewares/authMiddleware.js
+++ src/app/middlewares/authMiddleware.js
@@ -3,17 +3,29 @@ const { promisify } = require('util');
 const configAuth = require('../../config/configAuth');
 
 module.exports = async (req, res, next) => {
+  /**
+   * pegando o token de atenticaão do header;
+   */
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
     return res.status(401).json({ error: 'Token não encontrado.' });
   }
 
+  /**
+   * desestruturando o array pra pegar a segunda posição onde fica o token
+   */
   const [, token] = authHeader.split(' ');
 
   try {
+    /**
+     * transforma a função verify pra usar o async/await
+     */
     const decoded = await promisify(jwt.verify)(token, configAuth.secret);
 
+    /**
+     * envia o token na variável userId pela requisição p/ que possa ser usada
+     */
     req.userId = decoded.id;
 
     return next();
